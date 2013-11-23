28bd69f164b1de55b9223f0467fabf342fb408ac
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-23 20:17:50 +0000

    Returns array of most-used language per repo

--- github_user_language/app.rb
+++ github_user_language/app.rb
@@ -5,4 +5,11 @@ user = gets.chomp
 
 response =  Net::HTTP.get_response(URI("https://api.github.com/users/#{user}/repos"))
 
-puts JSON.parse(response.body)
+# response =  Net::HTTP.get_response(URI("https://api.github.com/repos/#{user}/QuizApp/languages"))
+
+
+user_array = JSON.parse(response.body)
+
+repos = user_array.map{ |repo| repo["language"] }
+
+puts repos
