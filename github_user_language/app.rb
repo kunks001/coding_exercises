24ad4cbf0ce717a898805bcd14380affd2443fe8
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-23 22:01:02 +0000

    script returns array of bytes per language per repo

--- github_user_language/app.rb
+++ github_user_language/app.rb
@@ -5,11 +5,17 @@ user = gets.chomp
 
 response =  Net::HTTP.get_response(URI("https://api.github.com/users/#{user}/repos"))
 
-# response =  Net::HTTP.get_response(URI("https://api.github.com/repos/#{user}/QuizApp/languages"))
-
-
 user_array = JSON.parse(response.body)
 
-repos = user_array.map{ |repo| repo["language"] }
+repos = user_array.map{ |repo| repo["name"] }
 
-puts repos
+languages_array = []
+
+repos.each do |repo|
+	repo_languages =  Net::HTTP.get_response(URI("https://api.github.com/repos/kunks001/#{repo}/languages"))
+	repo_languages_hash = JSON.parse(repo_languages.body)
+
+	languages_array << repo_languages_hash
+end
+
+puts languages_array
