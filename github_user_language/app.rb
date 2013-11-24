375b14a2457aa3c5ab1879bbe5e7abb6c4bb8252
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-24 18:40:50 +0000

    added spec folder to github_language

--- github_user_language/app.rb
+++ github_user_language/app.rb
@@ -1,6 +1,8 @@
 require 'net/http'
 require 'json'
 
+module GithubLanguage
+
 user = gets.chomp
 
 response =  Net::HTTP.get_response(URI("https://api.github.com/users/#{user}/repos"))
@@ -19,3 +21,24 @@ repos.each do |repo|
 end
 
 puts languages_array
+
+end
+
+# user = gets.chomp
+
+# response =  Net::HTTP.get_response(URI("https://api.github.com/users/#{user}/repos"))
+
+# user_array = JSON.parse(response.body)
+
+# repos = user_array.map{ |repo| repo["name"] }
+
+# languages_array = []
+
+# repos.each do |repo|
+# 	repo_languages =  Net::HTTP.get_response(URI("https://api.github.com/repos/kunks001/#{repo}/languages"))
+# 	repo_languages_hash = JSON.parse(repo_languages.body)
+
+# 	languages_array << repo_languages_hash
+# end
+
+# puts languages_array
