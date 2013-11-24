edbaaebae26f1d926b5af56c2c21c60092a5f2b1
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-24 22:45:01 +0000

    attempting to get github hash for testing

---
+++ github_user_language/example.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'json'
+
+user = gets.chomp
+
+response =  Net::HTTP.get_response(URI("https://api.github.com/users/#{user}/repos"))
+
+user_array = JSON.parse(response.body)
+
+repos = user_array.map{ |repo| repo["language"] }
+
+languages_array = []
+
+repos.each do |repo|
+	repo_languages =  Net::HTTP.get_response(URI("https://api.github.com/repos/kunks001/#{repo}/languages"))
+	repo_languages_hash = JSON.parse(repo_languages.body)
+
+	languages_array << repo_languages_hash
+end
+
+puts languages_array
