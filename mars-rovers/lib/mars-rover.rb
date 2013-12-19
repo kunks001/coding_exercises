1b63dd0d72dcfe7f7c00f2e7e72b9328224ef7c8
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-19 18:48:42 +0000

    sorted out indentation

--- mars-rovers/lib/mars-rover.rb
+++ mars-rovers/lib/mars-rover.rb
@@ -1,13 +1,12 @@
 class MarsRover
 
-	attr_reader :x_coord
-	attr_reader :y_coord
-	attr_reader :direction
+  attr_reader :x_coord
+  attr_reader :y_coord
+  attr_reader :direction
 
-	def initialize(x=3,y=3,d='N')
-		@x_coord=x
-		@y_coord=y
-		@direction=d
-	end
-
+  def initialize(x=3,y=3,d='N')
+    @x_coord=x
+    @y_coord=y
+    @direction=d
+  end
 end
