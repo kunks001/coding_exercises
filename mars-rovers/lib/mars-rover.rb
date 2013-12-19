91a622df9215eddd2e1f98fba3f754c14d622a51
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-19 18:20:54 +0000

    added starting direction for rover

--- mars-rovers/lib/mars-rover.rb
+++ mars-rovers/lib/mars-rover.rb
@@ -2,10 +2,12 @@ class MarsRover
 
 	attr_reader :x_coord
 	attr_reader :y_coord
+	attr_reader :direction
 
-	def initialize(x,y)
+	def initialize(x=3,y=3,d='N')
 		@x_coord=x
 		@y_coord=y
+		@direction=d
 	end
 
 end
--- mars-rovers/spec/mars-rover_spec.rb
+++ mars-rovers/spec/mars-rover_spec.rb
@@ -5,11 +5,15 @@ describe MarsRover do
   context 'a new MarsRover' do
 
     it 'should know its starting x-coordinate' do
-    	expect(MarsRover.new(3,2).x_coord).to eq 3
+    	expect(MarsRover.new(3,2,'N').x_coord).to eq 3
     end
 
     it 'should know its starting y-coordinate' do
-    	expect(MarsRover.new(3,2).y_coord).to eq 2
+    	expect(MarsRover.new(3,2,'N').y_coord).to eq 2
+    end
+
+    it 'should know its starting direction' do
+      expect(MarsRover.new(3,2,'N').direction).to eq 'N'
     end
   end
 end
