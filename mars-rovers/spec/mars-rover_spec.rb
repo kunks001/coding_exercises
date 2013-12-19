727d4ce6b9ee90009ed17bb1820c0a5816162c25
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-19 20:51:49 +0000

    refactored rover spec, reverted direction spec for readability

--- mars-rovers/spec/mars-rover_spec.rb
+++ mars-rovers/spec/mars-rover_spec.rb
@@ -3,6 +3,10 @@ require './lib/direction'
 
 describe MarsRover do
 
+  def mars_rover_facing(x, y, direction)
+    @rover = MarsRover.new(x,y, direction)
+  end
+
   context 'a new MarsRover' do
 
     it 'should know its starting x-coordinate' do
@@ -21,77 +25,77 @@ describe MarsRover do
   context 'when turning right' do
 
     it "should start facing north, end up facing east" do
-      marsRover=MarsRover.new(3,3,'N')
-      marsRover.turn_right
-      expect(marsRover.direction).to eq 'E'
+      mars_rover_facing(3,3,'N')
+      @rover.turn_right
+      expect(@rover.direction).to eq 'E'
     end
 
     it "should start facing east, end up facing south" do
-      marsRover=MarsRover.new(3,3,'E')
-      marsRover.turn_right
-      expect(marsRover.direction).to eq 'S'
+      mars_rover_facing(3,3,'E')
+      @rover.turn_right
+      expect(@rover.direction).to eq 'S'
     end
 
     it "should start facing south, end up facing west" do
-      marsRover=MarsRover.new(3,3,'S')
-      marsRover.turn_right
-      expect(marsRover.direction).to eq 'W'
+      mars_rover_facing(3,3,'S')
+      @rover.turn_right
+      expect(@rover.direction).to eq 'W'
     end
 
     it "should start facing west, end up facing north" do
-      marsRover=MarsRover.new(3,3,'W')
-      marsRover.turn_right
-      expect(marsRover.direction).to eq 'N'
+      mars_rover_facing(3,3,'W')
+      @rover.turn_right
+      expect(@rover.direction).to eq 'N'
     end
   end
 
   context 'when turning left' do
 
     it "should start facing north, end up facing west" do
-      marsRover=MarsRover.new(3,3,'N')
-      marsRover.turn_left
-      expect(marsRover.direction).to eq 'W'
+      mars_rover_facing(3,3,'N')
+      @rover.turn_left
+      expect(@rover.direction).to eq 'W'
     end
 
     it "should start facing west, end up facing south" do
-      marsRover=MarsRover.new(3,3,'W')
-      marsRover.turn_left
-      expect(marsRover.direction).to eq 'S'
+      mars_rover_facing(3,3,'W')
+      @rover.turn_left
+      expect(@rover.direction).to eq 'S'
     end
 
     it "should start facing south, end up facing east" do
-      marsRover=MarsRover.new(3,3,'S')
-      marsRover.turn_left
-      expect(marsRover.direction).to eq 'E'
+      mars_rover_facing(3,3,'S')
+      @rover.turn_left
+      expect(@rover.direction).to eq 'E'
     end
 
     it "should start facing east, end up facing north" do
-      marsRover=MarsRover.new(3,3,'E')
-      marsRover.turn_left
-      expect(marsRover.direction).to eq 'N'
+      mars_rover_facing(3,3,'E')
+      @rover.turn_left
+      expect(@rover.direction).to eq 'N'
     end
   end
 
   context "when moving" do
 
     it "should increase its y-coordinate if facing north" do
-      marsRover=MarsRover.new(3,3,'N')
-      expect{ marsRover.move }.to change { marsRover.y_coord }.by(1)
+      mars_rover_facing(3,3,'N')
+      expect{ @rover.move }.to change { @rover.y_coord }.by(1)
     end
 
     it "should decrease its y-coordinate if facing south" do
-      marsRover=MarsRover.new(3,3,'S')
-      expect{ marsRover.move }.to change { marsRover.y_coord }.by(-1)
+      mars_rover_facing(3,3,'S')
+      expect{ @rover.move }.to change { @rover.y_coord }.by(-1)
     end
 
     it "should increase its x-coordinate if facing east" do
-      marsRover=MarsRover.new(3,3,'E')
-      expect{ marsRover.move }.to change { marsRover.x_coord }.by(1)
+      mars_rover_facing(3,3,'E')
+      expect{ @rover.move }.to change { @rover.x_coord }.by(1)
     end
 
     it "should decrease its x-coordinate if facing west" do
-      marsRover=MarsRover.new(3,3,'W')
-      expect{ marsRover.move }.to change { marsRover.x_coord }.by(-1)
+      mars_rover_facing(3,3,'W')
+      expect{ @rover.move }.to change { @rover.x_coord }.by(-1)
     end
 
   end
