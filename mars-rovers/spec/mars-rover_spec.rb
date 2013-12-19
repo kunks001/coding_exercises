b02c8967d4007ceef15b19a47b2e88bdc8666c0e
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-19 19:38:04 +0000

    mars rover can turn right

--- mars-rovers/spec/mars-rover_spec.rb
+++ mars-rovers/spec/mars-rover_spec.rb
@@ -25,5 +25,23 @@ describe MarsRover do
       marsRover.turn_right
       expect(marsRover.direction).to eq 'E'
     end
+
+    it "should start facing east, end up facing south" do
+      marsRover=MarsRover.new(3,3,'E')
+      marsRover.turn_right
+      expect(marsRover.direction).to eq 'S'
+    end
+
+    it "should start facing south, end up facing west" do
+      marsRover=MarsRover.new(3,3,'S')
+      marsRover.turn_right
+      expect(marsRover.direction).to eq 'W'
+    end
+
+    it "should start facing west, end up facing north" do
+      marsRover=MarsRover.new(3,3,'W')
+      marsRover.turn_right
+      expect(marsRover.direction).to eq 'N'
+    end
   end
 end
