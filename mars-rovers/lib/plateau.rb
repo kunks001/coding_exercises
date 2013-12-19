1bdb6eb5fcf34534217c5430d8c48695f791afff
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-19 18:17:51 +0000

    refactored plateau.rb

--- mars-rovers/lib/plateau.rb
+++ mars-rovers/lib/plateau.rb
@@ -1,15 +1,10 @@
 class Plateau
 
+	attr_reader :width
+	attr_reader :height
+
 	def initialize(w=5, h=5)
 		@width = w
 		@height = h
-	end
-
-	def width
-		@width
-	end
-
-	def height
-		@height
 	end
 end
